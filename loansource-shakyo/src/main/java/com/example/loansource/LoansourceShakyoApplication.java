a30eaaabee1de738e13a0ca60653596463b0aec2
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2021-05-12 17:32:42 +0900

    first commit

---
+++ loansource-shakyo/src/main/java/com/example/loansource/LoansourceShakyoApplication.java
@@ -0,0 +1,13 @@
+package com.example.loansource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansourceShakyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansourceShakyoApplication.class, args);
+	}
+
+}
---
+++ loansource-shakyo/src/test/java/com/example/loansource/LoansourceShakyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loansource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansourceShakyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
