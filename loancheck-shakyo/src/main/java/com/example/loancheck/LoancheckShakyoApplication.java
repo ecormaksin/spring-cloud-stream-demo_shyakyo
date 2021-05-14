dc4bbb7c23bd202a532471706eabd27d1625f62c
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2021-05-14 18:00:40 +0900

    [WIP] Receiving Events

--- loancheck-shakyo/src/main/java/com/example/loancheck/LoancheckShakyoApplication.java
+++ loancheck-shakyo/src/main/java/com/example/loancheck/LoancheckShakyoApplication.java
@@ -1,13 +1,19 @@
 package com.example.loancheck;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(LoanProcessor.class)
 public class LoancheckShakyoApplication {
+
+	public static final Logger log = LoggerFactory.getLogger(LoancheckShakyoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoancheckShakyoApplication.class, args);
+		log.info("The Loancheck Application has started...");
 	}
-
 }
